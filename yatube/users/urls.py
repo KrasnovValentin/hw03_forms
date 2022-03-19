a254acbdd25a83eed79c7bf4b604299c481a5e4c
Валентин Краснов <valkred@gmail.com>
2022-03-19 18:08:06 +0300

    YaPrkt Check PEP8-1

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,11 +1,13 @@
-from django.contrib.auth.views import *
+from django.contrib.auth.views import LogoutView, LoginView, \
+    PasswordChangeView, PasswordResetView
 from django.urls import path
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    path('signup/', views.SignUp.as_view(template_name='users/signup.html'), name='signup'),
+    path('signup/', views.SignUp.as_view(template_name='users/signup.html'),
+         name='signup'),
     path(
         'logout/',
         LogoutView.as_view(template_name='users/logged_out.html'),
@@ -18,33 +20,15 @@ urlpatterns = [
     ),
     path(
         'auth/password_change/',
-        PasswordChangeView.as_view(template_name='users/password_change_form.html'),
+        PasswordChangeView.as_view(
+            template_name='users/password_change_form.html'),
         name='pass_ch_v'
     ),
-    path(
-        'auth/password_change/done/',
-        PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
-        name='pass_ch_d'
-    ),
+
     path(
         'auth/password_reset/',
-        PasswordResetView.as_view(template_name='users/password_reset_form.html'),
+        PasswordResetView.as_view(
+            template_name='users/password_reset_form.html'),
         name='pass_res'
     ),
-    path(
-        'auth/password_reset/done/',
-        PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
-        name='pass_res_d'
-    ),
-    path(
-        'auth/reset/<uidb64>/<token>',
-        PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
-        name='pass_res_conf'
-    ),
-    path(
-        'auth/reset/done',
-        PasswordResetView.as_view(template_name='users/password_reset_complete.html'),
-        name='pass_res_compl'
-    ),
-
 ]
